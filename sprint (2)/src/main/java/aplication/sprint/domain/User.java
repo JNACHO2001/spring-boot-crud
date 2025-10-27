5b62982dd5b8e587bb92fbc1ce459b136121724f
Coder <Coder@usuario-Saturno-Series>
2025-10-27 18:54:07 -0500

    feat:creacion de la entidad user

---
+++ sprint (2)/src/main/java/aplication/sprint/domain/User.java
@@ -0,0 +1,76 @@
+package aplication.sprint.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    private int id ;
+    private String nombre;
+    private int edad;
+    private String email;
+    private Long telefono;
+
+    public User() {
+    }
+    
+    
+
+    public User(int id, String nombre, int edad, String email, Long telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.email = email;
+        this.telefono = telefono;
+    }
+    
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Long telefono) {
+        this.telefono = telefono;
+    }
+    
+
+}
