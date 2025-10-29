88c1ca267151c938b6905311113c69e16f064752
Coder <Coder@usuario-Saturno-Series>
2025-10-29 18:20:54 -0500

    feat:terminacion de de los controladores

--- sprint (2)/src/main/java/aplication/sprint/controlador/EventControlador.java
+++ sprint (2)/src/main/java/aplication/sprint/controlador/EventControlador.java
@@ -4,35 +4,53 @@ import aplication.sprint.service.EventServiceIm;
 import aplication.sprint.web.dto.EventRequest;
 import aplication.sprint.web.dto.EventResponse;
 import jakarta.validation.Valid;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RestController
 @RequestMapping("api/event")
 public class EventControlador {
-    
-    
+
     private final EventServiceIm service;
 
     public EventControlador(EventServiceIm service) {
         this.service = service;
     }
-    
-    
-    
+
     @PostMapping
-    public ResponseEntity<EventResponse>  crear(@Valid  @RequestBody EventRequest req ){
-    var res = service.crear(req);
-     return  ResponseEntity.status(HttpStatus.CREATED).body(res);
-    
+    public ResponseEntity<EventResponse> crear(@Valid @RequestBody EventRequest req) {
+        var res = service.crear(req);
+        return ResponseEntity.status(HttpStatus.CREATED).body(res);
+
     }
-    
-    
+
+    @GetMapping
+    public ResponseEntity<List<EventResponse>> listar() {
+
+        return ResponseEntity.ok(service.listar());
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EventResponse> obtener(@PathVariable Integer id) {
+
+        return ResponseEntity.ok(service.obtenerPorId(id));
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> eliminar(@PathVariable Integer id) {
+        service.Eliminar(id);
+        return ResponseEntity.ok("eliminado");
+
+    }
 
 }
